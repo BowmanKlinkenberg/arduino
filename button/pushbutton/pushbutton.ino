5aeb850fd923811d0984eb840b78333e2b0820b6
Bowman Klinkenberg <bowman.klinkenberg@target.com>
2018-05-09 08:58:20 -0500

    initial projects

---
+++ button/pushbutton/pushbutton.ino
@@ -0,0 +1,16 @@
+const int inputPin = 2;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(inputPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int status = digitalRead(inputPin);
+  if (status == HIGH) {
+   digitalWrite(ledPin, HIGH);
+  } else {
+   digitalWrite(ledPin, LOW);
+  }
+}
